16360762f049a0e9151835b97a2b8e643b932194
Euller da Silva Ferreira <euler.ferreira19@gmail.com>
2025-03-19 08:05:20 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import ListUsers from "./pages/listUsers";
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/users" element={<ListUsers />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+export default App;
